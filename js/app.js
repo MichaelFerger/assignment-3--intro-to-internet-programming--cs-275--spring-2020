5a1263270d609b33cd04b68710d8415b6507f6e0
MichaelFerger <60191614+MichaelFerger@users.noreply.github.com>
2020-05-09 17:13:14 -0400

    Add click event listener that rotates A 180deg when clicked

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@ let rotatedA = false;
 let rotatedB = false;
 let rotatedC = false;
 let rotatedD = false;
+
+letters[0].addEventListener(`click`, function(){
+    rotatedB = false;
+    rotatedC = false;
+    rotatedD = false;
+    let deg = rotatedA ? 0 : 180;
+    letters[0].style.transform = `rotate(`+deg+`deg)`;
+    rotatedA = !rotatedA;
+});
